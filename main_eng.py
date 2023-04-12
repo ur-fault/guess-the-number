0b2f58b8fa208657e1b440376d06f9eb803db30a
morsee <42314964+ur-fault@users.noreply.github.com>
2023-04-12 19:20:21 +0200

    Fix: typo

--- main_eng.py
+++ main_eng.py
@@ -7,4 +7,4 @@ while (inp := input('1 - guess the number\n2 - think of a number\n3 - exit\n> ')
 	if inp == ('2', window := (1, 100), guess_count := 0)[0]:
 		while (ans := input(f'I guess {(window[0] + window[1]) // 2}, is the number "smaller", "bigger" or "right"? ').lower()) != 'right':
 			guess_count += 1; window = (window[0], (window[0] + window[1]) // 2) if ans == 'smaller' else ((window[0] + window[1]) // 2, window[1]) if ans == 'bigger' else (window, print(f'expected: "smaller", "bigger", "right"'), guess_count := guess_count - 1)[0]
-	if inp in ('1', '2'): print(f'right, the number of attempts: {guess_count}')
+	if inp in ('1', '2'): print(f'correct, the number of attempts: {guess_count}')

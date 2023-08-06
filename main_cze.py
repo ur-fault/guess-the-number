fc30ad3c7a69dca0202858e4d03c66029d480baa
morsee <42314964+ur-fault@users.noreply.github.com>
2023-08-06 17:12:55 +0200

    Fix: in mode 1, incorrect guess count

--- main_cze.py
+++ main_cze.py
@@ -4,7 +4,7 @@ while (inp := input('1 - hádej číslo\n2 - mysli na číslo\n3 - exit\n> ').lo
 		while (guess := input('tvůj tip: ')) != str(num):
 			try: print(f'číslo je menší' if int(guess) > num else 'číslo je větší'); guess_count += 1
 			except ValueError: print(f'"{guess}" není celé číslo'); guess_count -= 1
-	if inp == ('2', window := (1, 100), guess_count := 0)[0]:
+	if inp == ('2', window := (1, 100)):
 		while (ans := input(f'hádám {(window[0] + window[1]) // 2}, je číslo "menší", "větší" nebo "správně"? ').lower()) != 'správně':
 			guess_count += 1; window = (window[0], (window[0] + window[1]) // 2) if ans == 'menší' else ((window[0] + window[1]) // 2, window[1]) if ans == 'větší' else (window, print(f'očekávano: "menší", "větší", "správně"'), guess_count := guess_count - 1)[0]
 	if inp in ('1', '2'): print(f'správně, počet pokusů: {guess_count}')
